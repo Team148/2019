55b70080faf363012b323b9f45a8e94008a07a72
Caleb Bryan <35179811+CBry148@users.noreply.github.com>
2019-03-05 20:11:13 -0600

    Unbag 3

--- 2019/src/main/java/frc/robot/Constants.java
+++ 2019/src/main/java/frc/robot/Constants.java
@@ -25,13 +25,13 @@ public class Constants {
     public static final double ELEVATOR_UP_OUTPUT_PERCENT = 1.0;
     public static final double ELEVATOR_DOWN_OUTPUT_PERCENT = -0.4;
 
-    public static final double ELEVATOR_F_UP = 0.1367;
-    public static final double ELEVATOR_F_DOWN = ELEVATOR_F_UP - 0.025;
+    public static final double ELEVATOR_F_UP = 0.08;
+    public static final double ELEVATOR_F_DOWN = ELEVATOR_F_UP - 0.030;
     public static final double ELEVATOR_ZERO_F = -0.0732;
     // public static final double ELEVATOR_ZERO_F = 0.0;
-    public static final double ELEVATOR_P = 0.037;
+    public static final double ELEVATOR_P = 0.04;
     public static final double ELEVATOR_I = 0.0;
-    public static final double ELEVATOR_D = 0.037;
+    public static final double ELEVATOR_D = 0.4;
 
     public static final double ELEVATOR_F_VELOCITY = 0.26;
     public static final double ELEVATOR_P_VELOCITY = 0.7;
